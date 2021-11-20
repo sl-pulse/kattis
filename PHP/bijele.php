06f37fd2fcff337c8a6573960e983cde1fa06947
SL <90300190+sl-pulse@users.noreply.github.com>
2021-11-21 02:36:22 +0800

    Problem ID: bijele
    https://open.kattis.com/problems/bijele

---
+++ PHP/bijele.php
@@ -0,0 +1,12 @@
+<?php
+//https://open.kattis.com/problems/bijele
+//Running Time: 0.01s
+while (fscanf(STDIN, '%d%d%d%d%d%d', $a, $b, $c, $d, $e, $f )==6){
+    echo (1-$a)."\n";
+    echo (1-$b)."\n";
+    echo (2-$c)."\n";
+    echo (2-$d)."\n";
+    echo (2-$e)."\n";
+    echo (8-$f)."\n";
+   
+}
